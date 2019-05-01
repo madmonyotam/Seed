5de423a01ce46cd86854b2a96938f2164ad9a35a
Yotam Madmon <madmonyotam@gmail.com>
2019-05-01 15:28:38 +0300

    remove plugins without examples

--- source/plugins/widgetManager/index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = require('./widgetManager.js');
-
-if(module.hot) {
-    module.hot.accept('./widgetManager.js', function() {
-        var plugin = require('./widgetManager.js');
-        core.reloadPlugin(plugin);
-    });
-}
