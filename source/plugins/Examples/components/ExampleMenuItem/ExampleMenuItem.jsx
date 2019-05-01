b843d98a91f20ee96282be1aa88a7fca67eb7f1c
Zerr, Ziv <downzed@gmail.com>
2019-05-01 18:15:51 +0300

    better packaging
    REMOVE node_modules  and run `npm i`

--- source/plugins/Examples/components/ExampleMenuItem/ExampleMenuItem.jsx
+++ source/plugins/Examples/components/ExampleMenuItem/ExampleMenuItem.jsx
@@ -51,7 +51,7 @@ module.exports = {
       styles(s){
         const styles = {
           innerListItem:{
-            padding: "10px 10px",
+            padding: "10px 25px",
           },
         }
         return(styles[s]);
