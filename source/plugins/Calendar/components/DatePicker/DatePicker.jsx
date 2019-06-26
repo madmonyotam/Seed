23b9f3a968dc2d8fc4e341d9dc684742791c70a2
Yakov Rakhamimov <jacobra19@gmail.com>
2019-06-26 10:33:29 +0300

    DatePicker: add lower section

--- source/plugins/Calendar/components/DatePicker/DatePicker.jsx
+++ source/plugins/Calendar/components/DatePicker/DatePicker.jsx
@@ -119,7 +119,7 @@ module.exports = {
                             
                             
                             />
-                            {/* { this.renderButtons() } */}
+                            { this.renderButtons() }
                     </div>
 
                 )
